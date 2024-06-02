a6b5a6ca46a310b1dd4e46a403b568578508b141
Dawsh <dawsh@duck.com>
2024-06-02 10:07:31 +0330

    refactor(singbox): refactor and remove useless params

--- v2share/singbox.py
+++ v2share/singbox.py
@@ -70,141 +70,85 @@ class SingBoxConfig(str):
 
     @staticmethod
     def transport_config(
-        transport_type="",
-        host="",
-        path="",
-        method="",
-        headers={},
-        idle_timeout="15s",
-        ping_timeout="15s",
-        permit_without_stream=False,
+        transport_type="http",
+        host=None,
+        path=None,
+        http_method=None,
     ):
-        transport_config = {}
+        transport_config = {"type": transport_type}
 
-        if transport_type:
-            transport_config["type"] = transport_type
+        if transport_type == "http":
+            if host:
+                transport_config["host"] = host
+            if path:
+                transport_config["path"] = path
+            if http_method:
+                transport_config["method"] = http_method
+        elif transport_type == "ws":
+            if path:
+                if "?ed=" in path:
+                    path, max_early_data = path.split("?ed=")
+                    max_early_data = int(max_early_data)
+                    transport_config["early_data_header_name"] = (
+                        "Sec-WebSocket-Protocol"
+                    )
+                    transport_config["max_early_data"] = max_early_data
+                transport_config["path"] = path
+            if host:
+                transport_config["headers"] = {"Host": host}
+        elif transport_type == "grpc":
+            if path:
+                transport_config["service_name"] = path
 
-            if transport_type == "http":
-                if host:
-                    transport_config["host"] = host
-                if path:
-                    transport_config["path"] = path
-                if method:
-                    transport_config["method"] = method
-                if headers:
-                    transport_config["headers"] = headers
-                if idle_timeout:
-                    transport_config["idle_timeout"] = idle_timeout
-                if ping_timeout:
-                    transport_config["ping_timeout"] = ping_timeout
+        elif transport_type == "httpupgrade":
+            if host:
+                transport_config["host"] = host
+            if path:
+                transport_config["path"] = path
 
-            elif transport_type == "ws":
-                if path:
-                    if "?ed=" in path:
-                        path, max_early_data = path.split("?ed=")
-                        max_early_data = int(max_early_data)
-                        transport_config["early_data_header_name"] = (
-                            "Sec-WebSocket-Protocol"
-                        )
-                        transport_config["max_early_data"] = max_early_data
-                    transport_config["path"] = path
-                if host:
-                    headers.update({"Host": host})
-                if headers:
-                    transport_config["headers"] = headers
-
-            elif transport_type == "grpc":
-                if path:
-                    transport_config["service_name"] = path
-                if idle_timeout:
-                    transport_config["idle_timeout"] = idle_timeout
-                if ping_timeout:
-                    transport_config["ping_timeout"] = ping_timeout
-                if permit_without_stream:
-                    transport_config["permit_without_stream"] = permit_without_stream
-
-            elif transport_type == "httpupgrade":
-                if host:
-                    transport_config["host"] = host
-                if path:
-                    transport_config["path"] = path
-                if headers:
-                    transport_config["headers"] = headers
         return transport_config
 
-    @staticmethod
-    def make_outbound(
-        type: str,
-        remark: str,
-        address: str,
-        port: int,
-        net="ws",
-        path="",
-        host="",
-        flow="",
-        tls="",
-        sni="",
-        fp="",
-        alpn="",
-        pbk="",
-        sid="",
-        header_type="",
-        headers={},
-        ais=False,
-    ):
-        config = {
-            "type": type,
-            "tag": remark,
-            "server": address,
-            "server_port": port,
-        }
-        if flow and type == "vless":
-            config["flow"] = flow
-
-        if net in ["http", "ws", "quic", "grpc", "httpupgrade"]:
-            config["transport"] = SingBoxConfig.transport_config(
-                transport_type=net, host=host, path=path, headers=headers
-            )
-        else:
-            config["network"] = net
-
-        if tls in ("tls", "reality"):
-            config["tls"] = SingBoxConfig.tls_config(
-                sni=sni, fp=fp, tls=tls, pbk=pbk, sid=sid, alpn=alpn, ais=ais
-            )
-
-        return config
-
     def add_proxies(self, proxies: List[V2Data]):
-        for config in proxies:
-            outbound = self.make_outbound(
-                remark=config.remark,
-                type=config.protocol,
-                address=config.address,
-                port=config.port,
-                net=config.transport_type,
-                tls=config.tls,
-                flow=config.flow,
-                sni=config.sni,
-                host=config.host,
-                path=config.path,
-                alpn=config.alpn,
-                fp=config.fingerprint,
-                pbk=config.reality_pbk,
-                sid=config.reality_sid,
-                header_type=config.header_type,
-                headers={},
-                ais=config.allow_insecure,
-            )
+        for data in proxies:
+            outbound = {
+                "type": data.protocol,
+                "tag": data.remark,
+                "server": data.address,
+                "server_port": data.port,
+            }
+            if (
+                data.protocol == "vless"
+                and data.flow
+                and data.tls in ["tls", "reality"]
+            ):
+                outbound["flow"] = data.flow
 
-            if config.protocol in ["vless", "vmess"]:
-                outbound["uuid"] = str(config.uuid)
+            if data.transport_type in ["http", "ws", "quic", "grpc", "httpupgrade"]:
+                outbound["transport"] = SingBoxConfig.transport_config(
+                    transport_type=data.transport_type,
+                    host=data.host,
+                    path=data.path,
+                )
 
-            elif config.protocol == "trojan":
-                outbound["password"] = config.password
+            if data.tls in ("tls", "reality"):
+                outbound["tls"] = SingBoxConfig.tls_config(
+                    sni=data.sni,
+                    fp=data.fingerprint,
+                    tls=data.tls,
+                    pbk=data.reality_pbk,
+                    sid=data.reality_sid,
+                    alpn=data.alpn,
+                    ais=data.allow_insecure,
+                )
 
-            elif config.protocol == "shadowsocks":
-                outbound["password"] = config.password
-                outbound["method"] = config.shadowsocks_method
+            if data.protocol in ["vless", "vmess"]:
+                outbound["uuid"] = str(data.uuid)
+
+            elif data.protocol == "trojan":
+                outbound["password"] = data.password
+
+            elif data.protocol == "shadowsocks":
+                outbound["password"] = data.password
+                outbound["method"] = data.shadowsocks_method
 
             self._outbounds.append(outbound)
