054f8cd77f23d346029759221af47f3809998858
Dawsh <dawsh@duck.com>
2024-07-30 00:57:27 +0330

    fix: exceptions inheritance

--- v2share/exceptions.py
+++ v2share/exceptions.py
@@ -2,13 +2,13 @@ class V2ShareError(Exception):
     """Base class for all v2share exceptions"""
 
 
-class NotSupportedError(Exception):
+class NotSupportedError(V2ShareError):
     """indicating that the proxy is somehow not supported by v2share"""
 
 
-class TransportNotSupportedError(Exception):
+class TransportNotSupportedError(NotSupportedError):
     """indicating that the specified transport is not supported"""
 
 
-class ProtocolNotSupportedError(Exception):
+class ProtocolNotSupportedError(NotSupportedError):
     """indicating that the protocol is not supported"""
