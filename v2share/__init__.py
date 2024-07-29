2aedb21cb871e89c5cffde17a303d7dbb674248e
Dawsh <dawsh@duck.com>
2024-07-29 23:59:45 +0330

    refactor: all should only contain string

--- v2share/__init__.py
+++ v2share/__init__.py
@@ -4,4 +4,4 @@ from .data import V2Data
 from .singbox import SingBoxConfig
 from .xray import XrayConfig
 
-__all__ = [V2Data, SingBoxConfig, ClashConfig, ClashMetaConfig, XrayConfig]
+__all__ = ["V2Data", "SingBoxConfig", "ClashConfig", "ClashMetaConfig", "XrayConfig"]
